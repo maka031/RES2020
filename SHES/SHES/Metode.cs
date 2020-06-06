eb94334b560a079f3f998ca845f222f9ed12dd67
maka031 <mlazovic1998@gmail.com>
2020-06-06 16:54:08 +0200

    Funkcije

---
+++ SHES/SHES/Metode.cs
@@ -0,0 +1,17 @@
+﻿using Funkcije;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHES
+{
+    public class Metode : IMetode
+    {
+        public void TrenutnaSnagaPanela()
+        {
+            
+        }
+    }
+}
