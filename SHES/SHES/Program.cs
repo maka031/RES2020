75cff9561b882c1272bf8de7e7d93fff926384de
bojanpr412016 <64203034+bojanpr412016@users.noreply.github.com>
2020-06-09 11:07:49 +0200

    Baterija opet

--- SHES/SHES/Program.cs
+++ SHES/SHES/Program.cs
@@ -6,7 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SnagaSunca;
-using DodavanjeBaterije;
 
 namespace SHES
 {
@@ -17,42 +16,34 @@ namespace SHES
         {
             while (true)
             {
-
-
                 string pom = "";
-
 
                 Console.WriteLine("Izaberite opciju:");
                 Console.WriteLine("1. Dodaj novi panel");
                 Console.WriteLine("2. Dodaj bateriju");
                 Console.WriteLine("3. Izmeni snagu sunca:");
-                Console.WriteLine("4. Dodaj potrosac");
-                Console.WriteLine("5. Izlaz");
+                Console.WriteLine("4. Izlaz");
                 pom = Console.ReadLine();
                 int x = Int32.Parse(pom);
-
-
 
                 switch (x)
                 {
                     case 1:
                         dodavanjePanelaForm dp = new dodavanjePanelaForm();
-
                         dp.ShowDialog();
-
                         break;
                     case 2:
-                        dodavanjeBaterijeForm db = new dodavanjeBaterijeForm();
-                        db.ShowDialog();
-
                         break;
                     case 3:
                         Metode m = new Metode();
                         SnagaSuncaForm ss = new SnagaSuncaForm();
                         ss.ShowDialog();
-                        
                         break;
                     case 4:
+                        PotrosacForm pf = new PotrosacForm();
+                        pf.ShowDialog();
+                        break;
+                    case 5:
                         Application.Exit();
                         break;
                     default:
