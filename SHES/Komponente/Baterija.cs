7927264927ed6fd5cb65316edb47de07bb7d8090
maka031 <mlazovic1998@gmail.com>
2020-06-15 02:07:41 +0200

    Enum baterija

--- SHES/Komponente/Baterija.cs
+++ SHES/Komponente/Baterija.cs
@@ -7,17 +7,25 @@ using System.Threading.Tasks;
 
 namespace Komponente
 {
+    public enum Stanje
+    {
+        Punjenje,
+        Praznjenje
+    }
+
     public class Baterija
         
     {
         private string ime;
         private int maxSnaga;
         private int kapacitet;
+        private Stanje stanjeBaterije;
 
         [Key]
         public string Ime { get => ime; set => ime = value; }
         public int MaxSnaga { get => maxSnaga; set => maxSnaga = value; }
         public int Kapacitet { get => kapacitet; set => kapacitet = value; }
+        public Stanje StanjeBaterije { get => stanjeBaterije; set => stanjeBaterije = value; }
 
         public Baterija()
         {
